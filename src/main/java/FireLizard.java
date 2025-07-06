51a1aee67efeaa59f6f1c74a973e3a8003c9cd18
CodyCSUMB <codhopkins@csumb.edu>
2025-07-06 16:55:24 -0700

    setup lizard

--- src/main/java/FireLizard.java
+++ src/main/java/FireLizard.java
@@ -1,25 +1,33 @@
+import utilities.Dice;
+
 /*
  * @author Cody Hopkins
  * Date: 07/06/2025
  * Explanation:
  */
 public class FireLizard extends Monster {
+    private final int DEFENSE_MIN = 1;
+    private final int DEFENSE_MAX = 8;
+    private final int ATTACK_MIN = 8;
+    private final int ATTACK_MAX = 16;
+
     FireLizard(String value) {
         super(value, ElementalType.FIRE);
     }
 
     @Override
     public void setAttackPoints() {
+        this.setAttackPoints(Dice.roll(ATTACK_MIN, ATTACK_MAX));
 
     }
 
     @Override
     public void setDefensePoints(int defensePoints) {
-
+        this.defensePoints = defensePoints;
     }
 
     @Override
     public void setDefensePoints() {
-
+        this.setAttackPoints(Dice.roll(DEFENSE_MIN, DEFENSE_MAX));
     }
 }
